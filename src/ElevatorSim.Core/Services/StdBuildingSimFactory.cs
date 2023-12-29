88a029ce2ed82084c2a60c6dfd8c37681a452640
jaysen <jaysen@gmail.com>
2023-12-29 13:42:02 +0200

    BuildingSimFactory Interface and Std implementation

---
+++ src/ElevatorSim.Core/Services/StdBuildingSimFactory.cs
@@ -0,0 +1,17 @@
+﻿using ElevatorSim.Core.Models.Interfaces;
+using ElevatorSim.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevatorSim.Core.Services.Interfaces;
+
+namespace ElevatorSim.Core.Services;
+public class StdBuildingSimFactory : IBuildingSimFactory
+{
+    public IBuildingSim Create(int floorCount, int elevatorCount, int defaultCapacity, int defaultSpeed)
+    {
+        return new BuildingSim(floorCount, elevatorCount, defaultCapacity, defaultSpeed);
+    }
+}
